a7d0cdbb45cf09c49528b3329d6ccd877d9db8e0
jerfeson <jerfeson_guerreiro@hotmail.com>
2020-05-20 00:53:52 -0300

    :tada: first commit.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh -l
 
-
 service nginx start
 service php7.3-fpm restart
 
 service nginx status
 service php7.3-fpm status
 
-chmod -R 660 /github/workspace/data/keys/oauth/
+chmod 660 /github/workspace/data/keys/oauth/private.key
+chmod 660 /github/workspace/data/keys/oauth/public.key
+
+ls -l /github/workspace/data/keys/
 
 # Run codeception tests
 vendor/bin/codecept run $*
