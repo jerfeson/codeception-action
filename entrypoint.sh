dbaf7ebb144d9eef017bef923afa065478090b6d
jerfeson <jerfeson_guerreiro@hotmail.com>
2020-05-20 00:59:46 -0300

    :tada: first commit.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ service php7.3-fpm status
 chmod 660 /github/workspace/data/keys/oauth/private.key
 chmod 660 /github/workspace/data/keys/oauth/public.key
 
-ls -l /github/workspace/data/keys/
+ls -l /github/workspace/data/keys/oauth/
 
 # Run codeception tests
 vendor/bin/codecept run $*
